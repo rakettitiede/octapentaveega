7b39dfc48fcdf2eb29e979eecd77028e138fb1da
Jari Tulilahti <jari@rakettitiede.com>
2015-07-06 01:44:23 +0300

    59.9Hz

--- main.c
+++ main.c
@@ -11,22 +11,22 @@ volatile uint8_t screen[384] = { 72, 101, 108, 108, 111 };
 volatile uint16_t vline;
 uint16_t charindex = 0; 
 
-#define VMIN 61
+#define VMIN 62
 #define VMAX 445
 
 ISR(TIM1_COMPA_vect) {
 	static uint8_t alter = 0;
 	static uint8_t altcnt = 0;
-	uint8_t *lineptr;
+	volatile uint8_t *lineptr;
 
-	PORTB |= (1 << PB2);
-	__asm__ volatile(".rept 73\n\tnop\n\t.endr\n\t");
-	PORTB &= ~(1 << PB2);
+	PORTB |= (1 << PB2); // Hsync HIGH. Code should lose 70 cycles here
+	if (++vline == 525) vline = 0;
+	__asm__ volatile(".rept 59\n\tnop\n\t.endr\n\t");
+	PORTB &= ~(1 << PB2); // Hsync LOW
 
 	if (vline == 2) PORTB &= ~(1 << PB0);
 	if (vline == 0) PORTB |= (1 << PB0);
 
-	if (++vline == 524) vline = 0;
 
 	if (vline < VMIN || vline > VMAX) {
 		altcnt = 0;
@@ -285,9 +285,10 @@ int main(void) {
 	// Enable interrupts
 	sei();
 
-	for(uint8_t j = 0; j < 32; j++) line[j] = 31 << 3;
-	for(uint8_t j = 32; j < 64; j++) line[j] = 17 << 3;
+	for(uint8_t j = 0; j < 32; j++) line[j] = 10 << 3;
+	for(uint8_t j = 32; j < 64; j++) line[j] = 21 << 3;
 
 	for(;;) sleep_mode();
+//	for(;;); // this will jitter
 }
 
