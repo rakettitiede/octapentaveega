ba3a38634f1a2cc909b33be1679266e6338de035
Jari Tulilahti <jari@rakettitiede.com>
2015-07-07 19:09:12 +0300

    Wut

--- globals.h
+++ globals.h
@@ -1,13 +1,21 @@
 /*
  * Define global variables for both C and ASM code used
  */
+#define VMAX_VALUE 384
+#define VMAX_ASM VMAX_VALUE - 256
 
 #ifdef __ASSEMBLER__
 
+	#define sregstore r2
+	#define alter r3
+ 	#define VMAX VMAX_ASM
 
 #else
 
 #include <stdint.h>
 
+	register uint8_t sregstore __asm__("r2");
+	register uint8_t alter __asm__("r3");
+ 	#define VMAX VMAX_VALUE
 
 #endif
