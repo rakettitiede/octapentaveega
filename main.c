a7ba67d1cead26c2b089c3df2284c4c476483f7a
Jari Tulilahti <jari@rakettitiede.com>
2015-07-07 15:08:20 +0300

    Not working

--- main.c
+++ main.c
@@ -1,26 +1,53 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
+#include <avr/pgmspace.h>
 #include <util/delay.h>
 #include <string.h>
 #include <stdint.h>
 #include "font.h"
 
-volatile uint8_t line[65] = {};
-volatile uint8_t screen[384] = { 72, 101, 108, 108, 111, 32, 72, 101, 108, 108, 111, 32, 72, 101, 108, 108, 111, 32, 72, 101, 108, 108, 111, 32, 72, 101, 108, 108, 111, 32, 72, 101, 108, 108, 111, 32, 72, 101, 108, 108, 111, 32, 72, 101, 108, 108, 111, 32, 72, 101, 108, 108, 111, 32, 72, 101, 108, 108, 111, 32, 72, 101, 108, 108, 111, 32, 72, 101, 108, 108, 111, 32, 72, 101, 108, 108, 111, 32, 72, 101, 108, 108, 111, 32, 72, 101, 108, 108, 111, 32, 72, 101, 108, 108, 111, 32, 72, 101, 108, 108, 111, 32, 72, 101, 108, 108, 111, 32, 72, 101, 108, 108, 111, 32, 72, 101, 108, 108, 111, 32, 72, 101, 108, 108, 111, 32, 72, 101, 108, 108, 111, 32, 72, 101, 108, 108, 111, 32, };
-volatile uint16_t vline;
-uint16_t charindex = 0; 
+volatile uint8_t line[64] = {};
+volatile uint8_t screen[384] = { 
+	72, 101, 108, 108, 111, 32, 72, 101, 108, 108, 111, 32, 72, 101, 108, 108, 
+	111, 32, 72, 101, 108, 108, 111, 32, 72, 101, 108, 108, 111, 32, 72, 101, 
+	108, 108, 111, 32, 72, 101, 108, 108, 111, 32, 72, 101, 108, 108, 111, 32, 
+	72, 101, 108, 108, 111, 32, 72, 101, 108, 108, 111, 32, 72, 101, 108, 108, 
+	111, 32, 72, 101, 108, 108, 111, 32, 72, 101, 108, 108, 111, 32, 72, 101, 
+	108, 108, 111, 32, 72, 101, 108, 108, 111, 32, 72, 101, 108, 108, 111, 32, 
+	72, 101, 108, 108, 111, 32, 72, 101, 108, 108, 111, 32, 72, 101, 108, 108, 
+	111, 32, 72, 101, 108, 108, 111, 32, 72, 101, 108, 108, 111, 32, 72, 101, 
+	108, 108, 111, 32, 72, 101, 108, 108, 111, 32, };
+volatile uint16_t vline = 464;
+volatile uint16_t screen_index = 0;
+volatile uint8_t font_line = 0;
+volatile uint8_t char_x = 0;
 
 #define VMAX 384
 
 ISR(TIM1_COMPA_vect) {
-	static uint8_t alter = 0;
-	static uint8_t altcnt = 0;
+	static uint8_t alt = 0;
+	static uint8_t alt_cnt = 0;
 	volatile uint8_t *lineptr;
+	volatile uint8_t *fillptr;
+	volatile uint8_t *screenptr;
 
-	PORTB |= (1 << PB2); // Hsync HIGH. Code should lose 70 cycles here
-	__asm__ volatile(".rept 70\n\tnop\n\t.endr\n\t");
-	PORTB &= ~(1 << PB2); // Hsync LOW
+	fillptr = &line[(alt ^ 32) + char_x];
+	screenptr = &screen[screen_index];
+
+	/*
+	 * Create HSYNC. Spend 73 cycles between HIGH and LOW
+	 */
+	PORTB |= (1 << PB2); // HIGH
+
+	// Fill in 5 chars worth of bytes
+	*fillptr++ = pgm_read_byte(0x1807 + (*screenptr++));
+	*fillptr++ = pgm_read_byte(0x1807 + (*screenptr++));
+	*fillptr++ = pgm_read_byte(0x1807 + (*screenptr++));
+	*fillptr++ = pgm_read_byte(0x1807 + (*screenptr++));
+	*fillptr++ = pgm_read_byte(0x1807 + (*screenptr++));
+
+	PORTB &= ~(1 << PB2); // LOW
 
 	if (vline == 464) PORTB &= ~(1 << PB0);
 	if (vline == 462) PORTB |= (1 << PB0);
@@ -28,12 +55,12 @@ ISR(TIM1_COMPA_vect) {
 
 	if (vline > VMAX) {
 		if (vline == 525) vline = 0;
-		altcnt = 0;
-		alter = 0;
+		alt_cnt = 0;
+		alt = 0;
 		return;
 	}
 
-	lineptr = &line[alter];
+	lineptr = &line[alt];
 
 	USIDR = *lineptr++;
 	USICR |= (1 << USICLK);
@@ -259,9 +286,21 @@ ISR(TIM1_COMPA_vect) {
 	USICR |= (1 << USICLK);
 	USICR |= (1 << USICLK);
 
-	if (++altcnt == 4) {
-		alter ^= 32;
-		altcnt = 0;
+	char_x += 8;
+
+	// Fill in 3 more chars during back porch
+	// *fillptr++ = pgm_read_byte(0x1800 + (*screenptr++));
+	// *fillptr++ = pgm_read_byte(0x1800 + (*screenptr++));
+	// *fillptr++ = pgm_read_byte(0x1800 + (*screenptr++));
+
+	if (++alt_cnt == 4) {
+		char_x = 0;
+		alt ^= 32;
+		alt_cnt = 0;
+		screen_index += 8;
+		if (screen_index == 384) screen_index = 0;
+		// font_line++;
+		// font_line &= 0x07;
 	}
 }
 
@@ -277,8 +316,6 @@ int main(void) {
 	OCR1A = 158;
 	OCR1C = 158;
 	TIMSK |= (1 << OCIE1A);
-	TCCR0A = 0;
-	TCCR0B = (1 << CS00) | (1 << CS02);
 
 	// Sleep mode
 	set_sleep_mode(SLEEP_MODE_IDLE);
@@ -286,12 +323,8 @@ int main(void) {
 	// Enable interrupts
 	sei();
 
-	for(uint8_t j = 0; j < 32; j++) line[j] = 10 << 3;
-	for(uint8_t j = 32; j < 64; j++) line[j] = 21 << 3;
+	for(uint8_t i = 0; i < 64; i++) line[i] = 0;
 
-	for(;;) {
-		while(vline <= VMAX) sleep_mode();
-		while(vline) sleep_mode();
-	}
+	for(;;) sleep_mode();
 }
 
