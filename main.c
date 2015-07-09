66f82fdd74afc9ecc4a39e258febeaf657fb85a7
Jari Tulilahti <jari@rakettitiede.com>
2015-07-09 12:48:44 +0300

    Finally, first time text on screen. YAY.

--- main.c
+++ main.c
@@ -8,50 +8,66 @@
 #include "font.h"
 
 volatile uint8_t line[64] = {};
-volatile uint8_t screen[384] = { 
-	72, 101, 108, 108, 111, 32, 72, 101, 108, 108, 111, 32, 72, 101, 108, 108, 
-	111, 32, 72, 101, 108, 108, 111, 32, 72, 101, 108, 108, 111, 32, 72, 101, 
-	108, 108, 111, 32, 72, 101, 108, 108, 111, 32, 72, 101, 108, 108, 111, 32, 
-	72, 101, 108, 108, 111, 32, 72, 101, 108, 108, 111, 32, 72, 101, 108, 108, 
-	111, 32, 72, 101, 108, 108, 111, 32, 72, 101, 108, 108, 111, 32, 72, 101, 
-	108, 108, 111, 32, 72, 101, 108, 108, 111, 32, 72, 101, 108, 108, 111, 32, 
-	72, 101, 108, 108, 111, 32, 72, 101, 108, 108, 111, 32, 72, 101, 108, 108, 
-	111, 32, 72, 101, 108, 108, 111, 32, 72, 101, 108, 108, 111, 32, 72, 101, 
-	108, 108, 111, 32, 72, 101, 108, 108, 111, 32, };
+volatile unsigned char screen[384] = { 
+   //12345678901234567890123456789012
+	"Hello world. This is Attiny85   "
+	"displaying 32x12 characters on  "
+	"VGA with pure bitbang goodness.."
+	"      (C) 2015 by //Jartza      "
+	"--------------------------------"
+	"Here be dragons!    BEWARE!!!!!!"
+	"--------------------------------"
+	"            Rakettitiede rocks! "
+	"Rakettitiede rocks!             "
+	"================================"
+	"abcdefghijklmnoprstuvwxyz1234567"
+	"890,.-!\"#$%&/()+-ABCDEFGHIJKLMNO"	
+};
+
 volatile uint16_t vline = 464;
-volatile uint16_t screen_index = 0;
-volatile uint8_t font_line = 0;
-volatile uint8_t char_x = 0;
 
 #define VMAX 384
 
 ISR(TIM1_COMPA_vect) {
+	static uint16_t scr_buf_off = 0;
+	static uint8_t char_x = 0;
+	static uint8_t font_line = 0;
 	static uint8_t alt = 0;
 	static uint8_t alt_cnt = 0;
 	uint8_t *lineptr;
-	uint8_t *fillptr;
-	uint8_t *screenptr;
-
-	fillptr = &line[(alt ^ 32) + char_x];
-	screenptr = &screen[screen_index];
+	uint8_t *fillptr = &line[(alt ^ 32) + char_x];
+	uint8_t *screenptr = &screen[scr_buf_off + char_x];
+	static uint16_t font_addr = 0x1800;
 
 	/*
 	 * Create HSYNC. Spend 73 cycles between LOW and HIGH
 	 */
 	PORTB &= ~(1 << PB2); // HIGH
-	__asm__ volatile(".rept 73" "\t\n" "nop" "\t\n" ".endr" "\t\n");
+		*fillptr++ = pgm_read_byte(font_addr + (*screenptr++));
+		*fillptr++ = pgm_read_byte(font_addr + (*screenptr++));
+		*fillptr++ = pgm_read_byte(font_addr + (*screenptr++));
+		*fillptr++ = pgm_read_byte(font_addr + (*screenptr++));
+		*fillptr++ = pgm_read_byte(font_addr + (*screenptr++));
 	PORTB |= (1 << PB2); // LOW
 
-	if (vline == 464) PORTB |= (1 << PB0);
-	if (vline == 462) PORTB &= ~(1 << PB0);
 	vline++;
 
 	if (vline > VMAX) {
+		if (vline == 464) PORTB |= (1 << PB0);
+		if (vline == 462) PORTB &= ~(1 << PB0);
 		if (vline == 525) vline = 0;
 		alt_cnt = 0;
 		alt = 0;
+		char_x = 0;
+		scr_buf_off = 0;
+		font_line = 0;
+		font_addr = 0x1800;
 		return;
 	}
+
+	*fillptr++ = pgm_read_byte(font_addr + (*screenptr++));
+	*fillptr++ = pgm_read_byte(font_addr + (*screenptr++));
+	*fillptr++ = pgm_read_byte(font_addr + (*screenptr++));
 
 	lineptr = &line[alt];
 
@@ -251,19 +267,16 @@ ISR(TIM1_COMPA_vect) {
 
 	char_x += 8;
 
-	// Fill in 3 more chars during back porch
-	// *fillptr++ = pgm_read_byte(0x1800 + (*screenptr++));
-	// *fillptr++ = pgm_read_byte(0x1800 + (*screenptr++));
-	// *fillptr++ = pgm_read_byte(0x1800 + (*screenptr++));
-
 	if (++alt_cnt == 4) {
+		alt_cnt = 0;
 		char_x = 0;
 		alt ^= 32;
-		alt_cnt = 0;
-		screen_index += 8;
-		if (screen_index == 384) screen_index = 0;
-		// font_line++;
-		// font_line &= 0x07;
+		font_addr += 0x100;
+		if (++font_line == 0x08) {
+			font_line = 0;
+			scr_buf_off += 32;
+			font_addr = 0x1800;
+		}
 	}
 }
 
@@ -276,8 +289,8 @@ int main(void) {
 
 	// HSYNC timer. Prescaler 4, Compare value = 159 = 31.8us
 	TCCR1 = (1 << CTC1) | (1 << CS10) | (1 << CS11);
-	OCR1A = 158;
-	OCR1C = 158;
+	OCR1A = 162;
+	OCR1C = 162;
 	TIMSK |= (1 << OCIE1A);
 
 	// Sleep mode
@@ -285,15 +298,6 @@ int main(void) {
 
 	// Enable interrupts
 	sei();
-
-	for(uint8_t i = 0; i < 32; i = i + 2) {
-		line[i] = 0xA8;
-		line[i + 1] = 0x50;
-	}
-	for(uint8_t i = 32; i < 64; i = i + 2) {
-		line[i + 1] = 0xA8;
-		line[i] = 0x50;		
-	}
 
 	for(;;) sleep_mode();
 }
